e00dab091b61a6f446df2cd5e168811ff05041e2
akita066 <121979441+akita066@users.noreply.github.com>
2024-11-30 15:30:19 +0300

    Add files via upload

---
+++ project/src/config.js
@@ -0,0 +1,7 @@
+const config = {
+    targetHost: 'play.uo-enigma.com',
+    webhookUrl: 'https://discord.com/api/webhooks/1312392842034810921/iVnvMImefbciwsZP3PnxVDFoLlFjejM8A21hqwkuKlRbSlsKPBirbmjnXriMrFh-LYHw',
+    checkInterval: 60000, // 60 seconds
+};
+
+module.exports = config;
